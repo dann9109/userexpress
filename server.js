5ee4fc4a6d29f477c935720bc6cc8cfa500ac212
dann9109 <danielxstella@gmail.com>
2024-01-19 12:20:35 -0500

    added static files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const PORT = 3333;
 
@@ -20,6 +21,23 @@ async function saveUserData(usersArr) {
 
 // Opening up the middleware channel to allow json to be sent through from the client
 app.use(express.json());
+
+
+
+
+app.use(express.static('./public'));
+
+// opens cors to all domains
+app.use(cors());
+// app.get('/', (requestObj, responseObj) =>{
+//  responseObj.sendFile(path.join(__dirname, './public/index.html')) 
+// });
+
+// app.get('/css/style.css', (requestObj, responseObj) =>{
+//     responseObj.sendFile(path.join(__dirname, './public/css/style.css'))
+// })
+
+
 
 // Route to retreive/GET all users from the json database
 app.get('/api/users', async (requestObj, responseObj) => {
