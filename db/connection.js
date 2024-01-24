463276640d6542b90fa5e00bd90a962d45639686
dann9109 <danielxstella@gmail.com>
2024-01-24 13:44:16 -0500

    render

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
 
+
+require('dotenv').config();
+
+
+const is_prod = process.env.NODE_ENV === 'production';
+
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mysql_first_day_db'
+    host: process.env.DB_HOST_URL,
+    user: process.env.DB_USER,
+    password: is_prod ? process.env.DB_PASSWORD : '',
+    database: process.env.DB_DATABASE_NAME
 });
 
 module.exports = connection.promise();
